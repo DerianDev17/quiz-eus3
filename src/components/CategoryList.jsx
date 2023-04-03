22288117d881d9fd18273ac0a9aa2db30df138f5
Derian Nazareno <derian.nazareno@hotmail.com>
2023-04-03 17:56:30 -0500

    primer quiz-react

---
+++ src/components/CategoryList.jsx
@@ -0,0 +1,53 @@
+import { Link } from 'react-router-dom';
+import { imgs, categories } from '../data';
+import { CategoryCard } from './CategoryCard';
+
+const [
+	imgCiencia,
+	imgDeportes,
+	imgRazonamientoNumerico,
+	imgRazonamientoVerbal,
+	imgTecnologia,
+] = imgs;
+
+export const CategoryList = () => {
+	return (
+		<div className='flex flex-row flex-wrap justify-center gap-4 mt-10'>
+			{/* Category Link Razonamiento Logico*/}
+			<CategoryCard
+				category={categories.razonamientoLogico}
+				src={imgCiencia}
+				alt={`Categoría ${categories.razonamientoLogico}`}
+				gradientColor=' from-purple-500 to-pink-500'
+			/>
+			{/* Category Link Orientacion vocacional */}
+			<CategoryCard
+				category={categories.orientacionVocacional}
+				src={imgDeportes}
+				alt={`Categoría ${categories.orientacionVocacional}`}
+				gradientColor='from-lime-400 to-teal-700'
+			/>
+			{/* Category Link Razonamiento Numérico */}
+			<CategoryCard
+				category={categories.razonamientoNumerico}
+				src={imgRazonamientoNumerico}
+				alt={`Categoría ${categories.razonamientoNumerico}`}
+				gradientColor='from-red-400 to-zinc-400'
+			/>
+			{/* Category Link Razonamiento Verbal */}
+			<CategoryCard
+				category={categories.razonamientoVerbal}
+				src={imgRazonamientoVerbal}
+				alt={`Categoría ${categories.razonamientoVerbal}`}
+				gradientColor='from-amber-400 to-emerald-600'
+			/>
+			{/* Category Link Tecnologia */}
+			<CategoryCard
+				category={categories.tecnologia}
+				src={imgTecnologia}
+				alt={`Categoría ${categories.tecnologia}`}
+				gradientColor='from-violet-900 to-rose-500 '
+			/>
+		</div>
+	);
+};
